3910a3159627b3fc5bdaa04fd19cac1a7e48898b
bwagne32 <116775950+bwagne32@users.noreply.github.com>
2024-02-26 15:57:30 -0500

    Implemented motor test

---
+++ componentTesting/motorTest/motorTest.ino
@@ -0,0 +1,59 @@
+#include "var.hpp"
+
+// Motors //////////////////////////////////////////////////////////////////////////////////////////////////
+const uint8_t leftWheelPin1 = 7;
+const uint8_t leftWheelPin2 = 6;
+const uint8_t leftPWMpin = 5;
+
+const uint8_t rightWheelPin1 = 4;
+const uint8_t rightWheelPin2 = 3;
+const uint8_t rightPWMpin = 2;
+
+VAR_HPP_::motor left(leftWheelPin1, leftWheelPin2, leftPWMpin);
+VAR_HPP_::motor right(rightWheelPin1,rightWheelPin2,rightPWMpin);
+
+
+short out = 0;
+
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+uint16_t inputTime = 1000;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Ready");
+  
+  // Motors ////////////////////////////////////////////////////////////////////////////////
+  pinMode(leftWheelPin1, OUTPUT);
+  pinMode(leftWheelPin2, OUTPUT);
+  pinMode(leftPWMpin, OUTPUT);
+
+  pinMode(rightWheelPin1, OUTPUT);
+  pinMode(rightWheelPin2, OUTPUT);
+  pinMode(rightPWMpin, OUTPUT);
+
+}
+
+void loop() {
+  if(currentMillis - previousMillis >= inputTime){
+      if(Serial.available())
+        out = readInput();
+      //Serial.print("OUT: ");
+      //Serial.println(out);
+      if(out == 999){
+        left.brake();
+        right.brake();
+      }
+      else{
+      left.drive(out);
+      right.drive(out);
+      }
+      previousMillis = currentMillis;
+  }
+  
+  currentMillis = millis();
+}
+
+short readInput(){
+  return Serial.readStringUntil('\n').toInt();
+}
---
+++ componentTesting/motorTest/var.hpp
@@ -0,0 +1,65 @@
+#include <stdint.h>
+#include <cmath>
+#include "esp32-hal-gpio.h"
+#include "esp32-hal.h"
+// PID loop controls
+#ifndef VAR_HPP_
+#define VAR_HPP_
+
+// Motor class //////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+PWM   in1    in2
+0     0       0     Coast
+0     1       1     brake
+1     0       0     Coast
+1     1       0     Forward
+1     0       1     Reverse
+1     1       1     brake
+
+*/
+class motor {// I'm too lazy to make an actual cpp file for this so it'll be somewhat cramped
+public:
+  motor (const uint8_t& in1, const uint8_t& in2, const uint8_t& pwm){ in1_ = &in1; in2_ = &in2; pwm_ = &pwm; };
+  
+  void brake(){
+    manual(0,true,true);
+    Serial.println("BRAKE");
+  }
+
+  void drive(int signal){// receives input -255 to 255 for motor speed (0 for coast, negative for reverse, and positive for forward)
+    if (signal < 0){ // reverse
+      manual(signal, false, true);
+      Serial.println("Reverse");
+    }
+    else{ // forward
+      manual(signal, true, false); 
+      Serial.println("Forward");
+    }
+  };
+
+  void manual(short signal, bool in1, bool in2){
+    short output;
+    signal = std::abs(signal);
+    if(signal > 255)
+      output = 255;
+    else
+      output = signal;
+    
+    Serial.print("OUT:");
+    Serial.println(output);
+
+    analogWrite(*pwm_, output);
+    digitalWrite(*in1_, in1);
+    digitalWrite(*in2_, in2);
+  }
+
+
+private: // pointers to const pin references
+  const uint8_t *pwm_;
+  const uint8_t *in1_;
+  const uint8_t *in2_;
+};
+
+
+
+#endif // VAR_HPP_
